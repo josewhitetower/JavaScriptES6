e3aed37a2720743f9d2211bf8c86612056b377f7
Jose Torreblanca <josewhitetower@gmail.com>
2017-09-01 22:54:02 +0200

    Default params

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 ### Day 4: August 31, 2017    
 
 **Today's Progress**:   
-1. Today I learnt how to declare and use a constant
+1. Today I learnt how to declare and use a constant, let, default params
 
 **Thoughts**:     
 1.   
 
 **Link to work:**:     
-1. [Example](https://link)
+1. [Example](https://link) 
--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 window.onload = function() {
-    const pi = 3.14;
-    console.log(pi);
 
-    function calcArea(r) {
-        const pi = 10;
-        console.log("area:" + pi * r * r);
-    }
-    calcArea(5);
+
+    // const pi = 3.14;
+    // console.log(pi);
+
+    // function calcArea(r) {
+    //     const pi = 10;
+    //     console.log("area:" + pi * r * r);
+    // }
+    // calcArea(5);
+
 }
